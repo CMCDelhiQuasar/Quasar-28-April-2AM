fdaf1f181af97469214a8bb0e31d9c551bfa2a9e
Gufran Khurshid <gufran.khurshid@gmail.com>
2014-05-08 01:35:59 +0530

    Something Changed

--- QuasarTilesLayout/src/com/cmcdelhi/quasar/service/StudentService.java
+++ QuasarTilesLayout/src/com/cmcdelhi/quasar/service/StudentService.java
@@ -19,6 +19,8 @@ public class StudentService {
 
 	QuasarConnectionManager quasarConnectionManager;
 
+	//Something Changed
+	
 	public StudentService() {
 		quasarConnectionManager = QuasarConnectionManager.getInstance();
 	}
